c2e41e1504a8be1705689afe35790ed531fa4a8a
Anatolii <anatolii.isaiev@gmail.com>
2012-07-14 04:03:23 -0700

    added AstroLib library sources

---
+++ AndroidProject/src/com/mhuss/AstroLib/Latitude.java
@@ -0,0 +1,14 @@
+/*****************************************************************************\
+ * Latitude
+\*****************************************************************************/
+
+package com.mhuss.AstroLib;
+
+/**
+ * A simple class to create a type-safe Latitude
+ */
+public class Latitude {
+  public Latitude( double l ) { value = l; }
+
+  public double value;
+}
---
+++ AndroidProject/src/com/mhuss/AstroLib/Longitude.java
@@ -0,0 +1,14 @@
+/*****************************************************************************\
+ * Longitude
+\*****************************************************************************/
+
+package com.mhuss.AstroLib;
+
+/**
+ * A simple class to create a type-safe Longitude
+ */
+public class Longitude {
+  public Longitude( double l ) { value = l; }
+
+  public double value;
+}
