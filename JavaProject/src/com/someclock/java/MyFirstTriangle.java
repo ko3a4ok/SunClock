c81226310ef78f6e6b18a1f0cc7d830cb1fef85e
ko3a4ok <ko3a4ok.ua@gmail.com>
2012-07-14 13:06:59 +0300

    [ADD] libraries

---
+++ JavaProject/src/com/someclock/java/MyFirstTriangle.java
@@ -0,0 +1,42 @@
+package com.someclock.java;
+
+import com.badlogic.gdx.ApplicationListener;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ko3a4ok
+ * Date: 7/14/12
+ * Time: 1:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyFirstTriangle implements ApplicationListener {
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
